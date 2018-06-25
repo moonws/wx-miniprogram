57d59dc4c9eff4de037072fa78dc4334e2698e74
moonws <727712310@qq.com>
2018-06-25 21:44:41 +0800

    1
    详情界面

--- pages/index/shop_detail.js
+++ pages/index/shop_detail.js
@@ -11,20 +11,6 @@ Page({
     autoplay: true,
     interval: 5000,
     duration: 1000,
-    imgData: [
-      {
-        "imgUrl": "../../img/xianggua_detail1.png",
-        "id": "1"
-      }, {
-        "imgUrl": '../../img/xianggua_detail2.png',
-        "id": "2"
-
-      }, {
-        "imgUrl": '../../img/xianggua_detail3.png',
-        "id": "3"
-
-      }
-    ],
     productList: [
       {
         id: 1,
@@ -54,8 +40,30 @@ Page({
           }
         ],
         fansDes:"关注人数:",
-        fansNum:31153
-        }]
+        fansNum:31153,
+        }],
+        num:1,
+        // 使用data数据对象设置样式名  
+        minusStatus: 'disabled',
+        goodsDetail:[
+          {des:"供应商：",value:"菓然水果"},
+          { des: "品牌：", value: "无" },
+          { des: "规格：", value: "2至3个" },
+          { des: "产地：", value: "中国" },
+        ],
+        imagesDes: "强烈推荐：隔着屏幕闻着香味，百吃不厌的香瓜。基地直采，绿色种植甜香瓜，顾名思义又甜又香的瓜，厚实甜美的果肉，浓郁鲜美的香气，十分爽口，口感倍儿爽。吃出儿时的味道，带你回到小时候。",
+        place:"中国",
+        imagesUrl:[
+          { id: 1, imgUrl: "../../img/xg_imgDetail_01.png"},
+          { id: 2, imgUrl: "../../img/xg_imgDetail_02.png" },
+          { id: 3, imgUrl: "../../img/xg_imgDetail_03.png" }, 
+          { id: 4, imgUrl: "../../img/xg_imgDetail_04.png" },
+          { id: 5, imgUrl: "../../img/xg_imgDetail_05.png" },
+          { id: 6, imgUrl: "../../img/xg_imgDetail_06.png" },
+          { id: 7, imgUrl: "../../img/xg_imgDetail_07.png" },
+          { id: 8, imgUrl: "../../img/xg_imgDetail_08.png" },
+          { id: 9, imgUrl: "../../img/xg_imgDetail_09.png" }
+        ]
 
   },
 
@@ -141,5 +149,41 @@ Page({
    */
   onShareAppMessage: function () {
   
-  }
+  },
+  /* 点击减号 */
+  bindMinus: function () {
+    var num = this.data.num;
+    // 如果大于1时，才可以减  
+    if (num > 1) {
+      num--;
+    }
+    // 只有大于一件的时候，才能normal状态，否则disable状态  
+    var minusStatus = num <= 1 ? 'disabled' : 'normal';
+    // 将数值与状态写回  
+    this.setData({
+      num: num,
+      minusStatus: minusStatus
+    });
+  },
+  /* 点击加号 */
+  bindPlus: function () {
+    var num = this.data.num;
+    // 不作过多考虑自增1  
+    num++;
+    // 只有大于一件的时候，才能normal状态，否则disable状态  
+    var minusStatus = num < 1 ? 'disabled' : 'normal';
+    // 将数值与状态写回  
+    this.setData({
+      num: num,
+      minusStatus: minusStatus
+    });
+  },
+  /* 输入框事件 */
+  bindManual: function (e) {
+    var num = e.detail.value;
+    // 将数值与状态写回  
+    this.setData({
+      num: num
+    });
+  }  
 })
