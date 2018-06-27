fd29251745b1cad1fbd082f125fa9c5938c23ba4
moonws <727712310@qq.com>
2018-06-27 23:11:00 +0800

    购买记录
    购买记录

--- pages/index/shop_detail.js
+++ pages/index/shop_detail.js
@@ -11,6 +11,17 @@ Page({
     autoplay: true,
     interval: 5000,
     duration: 1000,
+    shopRecordData: [
+      { id: 1, headImg: '../../img/shopCar.png', userName: '黄**', shopNum: "2份", orderTime: "2018-06-27 12:01:17" },
+      { id: 2, headImg: '../../img/shopCar.png', userName: '王**', shopNum: "1份", orderTime: "2018-06-27 12:01:17" },
+      { id: 3, headImg: '../../img/shopCar.png', userName: '李**', shopNum: "2份", orderTime: "2018-06-27 12:01:17" },
+      { id: 4, headImg: '../../img/shopCar.png', userName: '吴*', shopNum: "5份", orderTime: "2018-06-27 12:01:17" },
+      { id: 5, headImg: '../../img/shopCar.png', userName: '赵**', shopNum: "1份", orderTime: "2018-06-27 12:01:17" },
+      { id: 6, headImg: '../../img/shopCar.png', userName: '韩**', shopNum: "1份", orderTime: "2018-06-27 12:01:17" },
+      { id: 7, headImg: '../../img/shopCar.png', userName: '何**', shopNum: "1份", orderTime: "2018-06-27 12:01:17" },
+      { id: 8, headImg: '../../img/shopCar.png', userName: '张*', shopNum: "1份", orderTime: "2018-06-27 12:01:17" },
+      { id: 9, headImg: '../../img/shopCar.png', userName: '甘**', shopNum: "2份", orderTime: "2018-06-27 12:01:17" }
+    ],
     productList: [
       {
         id: 1,
@@ -71,8 +82,7 @@ Page({
 
 
         ],
-        footImg:"../../img/detail_footer.png"
-
+        footImg:"../../img/detail_footer.png"    
   },
 
 
